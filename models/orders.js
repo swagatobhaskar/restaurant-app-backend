680b0fd3db8b55799d64b3a2b33f17c3886420e4
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-06-30 23:47:10 +0530

    working on orderSchema

--- models/orders.js
+++ models/orders.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-
+    menuItem: String,
+    totalPrice: mongoose.Types.Decimal128,
+    quantity: {
+        type: Number,
+        default: 1
+    }
 });
 
 const Order = mongoose.model('Order', orderSchema);
