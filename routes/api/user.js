83d0888b42bdffab5ee152394fe47c04380ea357
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-04 22:39:14 +0530

    resolved Promise { <pending> } issue with refresh token

--- routes/api/user.js
+++ routes/api/user.js
@@ -19,14 +19,14 @@ router.get('/list', Middlewares.authMiddleware, async(req, res) => {
 
 // PATH: /api/user/login
 // access: public
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
     User.findOne({email: req.body.email}, function(err, user) {
         if (err) throw err;
         if (user) {
-            user.comparePassword(req.body.password, function(err, isMatch) {
+            user.comparePassword(req.body.password, async function(err, isMatch) {
                 if (err) throw err;
                 //let token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {expiresIn: '24h'});
-                let tokens = utils.generateTokens(user._id);
+                let tokens = await utils.generateTokens(user._id);
                 console.log("Tokens:: ", tokens);
                 res.cookie('access', tokens.access, {maxAge: 60000, httpOnly: true, sameSite: 'lax'}) // secure: true
                 res.cookie('refresh', tokens.refresh, {maxAge: 24*3600*1000, httpOnly: true, sameSite: 'lax'}) // secure: true, 
--- utils/jwtTokens.js
+++ utils/jwtTokens.js
@@ -3,16 +3,16 @@ const User = require('../models/users');
 const RefreshToken = require('../models/refreshToken');
 const { v4: uuidv4 } = require('uuid');
 
-const generateTokens = (payload) => {
+const generateTokens = async (payload) => {
     const access = jwt.sign({payload}, process.env.SECRET_KEY, {
         expiresIn: "1h",
         jwtid: uuidv4(),    // required for refresh token as it points to one unique access token
         subject: String(payload),
     });
 
-    const refresh = RefreshToken.createToken(payload);
-
-    return token = {access, refresh};
+    let refresh = await RefreshToken.createToken(payload);
+    console.log("REF: ", refresh);
+    return token = { access, refresh };
 }
 
 const handleRefreshToken = async(req, res) => {
